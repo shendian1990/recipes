771d13f6b7b4052288eae94cf77df4d04e9dde6c
Shen Yanbin <shendian1990@gmail.com>
2019-05-09 17:48:46 +0800

    多线程单例

---
+++ c++11/noncopyable.cc
@@ -0,0 +1,12 @@
+
+class noncopyable
+{
+ public:
+  noncopyable(const noncopyable&) = delete;
+  void operator=(const noncopyable&) = delete;
+
+ protected:
+  noncopyable() = default;
+  virtual ~noncopyable() = default;
+};
+
